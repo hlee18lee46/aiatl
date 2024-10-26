75471ab810e05efdd1f364e5a40b8e91ab00f2e1
Han Lee <lee46@mail.usf.edu>
2024-10-26 11:49:01 -0400

    Initial Commit

---
+++ aiatl/aiatlApp.swift
@@ -0,0 +1,17 @@
+//
+//  aiatlApp.swift
+//  aiatl
+//
+//  Created by Han Lee on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct aiatlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aiatlTests/aiatlTests.swift
@@ -0,0 +1,17 @@
+//
+//  aiatlTests.swift
+//  aiatlTests
+//
+//  Created by Han Lee on 10/26/24.
+//
+
+import Testing
+@testable import aiatl
+
+struct aiatlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
